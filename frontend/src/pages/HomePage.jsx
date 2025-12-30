9a20426cd6cb57733082bed4efc96b38719b7362
samar-raj10 <silentkiller242810@gmail.com>
2025-12-30 21:17:31 +0530

    removed ratelimit-ui due to typo error

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,62 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar.jsx'
-import RateLimitUI from '../components/RateLImitUI.jsx';
-import axios from 'axios';
-import toast from 'react-hot-toast';
-import NoteCard from '../components/NoteCard.jsx';
-import TaskNotFound from '../components/TaskNotFound.jsx';
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar.jsx";
 
+import axios from "axios";
+import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard.jsx";
+import TaskNotFound from "../components/TaskNotFound.jsx";
 
 const HomePage = () => {
-  const [isRateLimit, setIsRateLimit] = useState(false);
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchNotes = async ()=> {
+    const fetchNotes = async () => {
       try {
         const res = await axios.get("http://localhost:5001/api/notes");
         console.log(res.data);
         setNotes(res.data);
-        setIsRateLimit(false)
       } catch (error) {
         console.log("Error fetching the Notes", error);
-        if(error.response.status === 429){
-          setIsRateLimit(true)
-        }
-        else{
-          toast.error("Failed to load notes")
-        }
+
+        toast.error("Failed to load notes");
+      } finally {
+        setLoading(false);
       }
-      finally{
-        setLoading(false)
-      }
-    }
+    };
 
     fetchNotes();
-  },[])
+  }, []);
   return (
     <div className="min-h-screen">
-      <Navbar/>
-
-      {isRateLimit && <RateLimitUI/>}
+      <Navbar />
 
       {/* <div className='text-center'><h1 className='text-2xl text-primary font-bold font-mono tracking-tight mt-6'>Upcoming Tasks</h1></div> */}
 
-      <div className='max-w-7xl mx-auto p-4 mt-6'>
-        {loading && <div className='text-center text-primary py-10'>Loading your tasks...</div>}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">
+            Loading your tasks...
+          </div>
+        )}
 
-        {notes.length === 0 && !isRateLimit && !loading && <TaskNotFound/>}
-        {notes.length > 0 && !isRateLimit && (
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-            {notes.map(note => (
-              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
+        {notes.length === 0  && !loading && <TaskNotFound />}
+        {notes.length > 0  && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
