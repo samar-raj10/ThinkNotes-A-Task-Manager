4540ce88cd53e45da32177959d096a12f3c97440
samar-raj10 <silentkiller242810@gmail.com>
2025-12-30 20:49:34 +0530

    completed for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,13 +12,17 @@ dotenv.config();
 //creating an express app
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware
-app.use(
-  cors({
-    origin: "http://localhost:8173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:8173",
+    })
+  );
+}
+
 app.use(express.json()); //used for accessing the req.body(parse the JSON body)
 app.use(rateLimiter);
 
@@ -28,6 +33,14 @@ app.use(rateLimiter);
 
 //router
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 //connecting to database and app listening to the specified port
 connectDB().then(() => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+//changes base url according to production or development mode
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
-})
+    baseURL: BASE_URL,
+});
 
 export default api
