813b6dd1ce1fa827009fe6c59efee1b82b3d95cc
samar-raj10 <silentkiller242810@gmail.com>
2025-12-30 22:37:33 +0530

    create README

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 //changes base url according to production or development mode
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://thinknotes-a-task-manager.onrender.com/api"
 const api = axios.create({
     baseURL: BASE_URL,
 });
